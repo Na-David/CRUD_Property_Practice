8ec24c64ffb826bff3669720aca42687ea78566b
DESKTOP-PB7NIIJ\hna20 <hna206@naver.com>
2022-07-11 22:59:31 +1200

    Add project files.

---
+++ CRUD_Pages/Models/Property.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUD_Pages.Models
+{
+    [Table("Property88")]
+    public partial class Property
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+    }
+}
